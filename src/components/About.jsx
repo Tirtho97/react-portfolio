5d92e27a7cf45388a44705ce363abd34699bee02
Tirthoraj <tirthoraj.1997@gmail.com>
2022-04-06 22:09:11 +0530

    About & Skills section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,6 +10,24 @@ const About = () => {
               About
             </p>
           </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold ">
+            <p>
+              Hello, my name is <span className="text-pink-600">Tirthoraj</span>
+              . Nice to meet you. Please take a look around.
+            </p>
+          </div>
+          <div>
+            <p className="text-[#8892b0]">
+              I am passionate about creating excellent softwares that improves
+              the life of people around me. I am a self-taught web developer
+              specializing in creating MERN stack applications. I am currently
+              looking for Software Developer roles which will push my
+              boundaries. Feel free to look into my works.
+            </p>
+          </div>
         </div>
       </div>
     </div>
